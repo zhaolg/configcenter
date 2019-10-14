4589f5a5d1e83c0ab916c6e487b704c98275a84c
lingang.zhaolg <lingang.zhao@ximalaya.com>
2019-10-14 21:56:33 +0800

    格式化数据

--- config-center-client/src/main/java/com/demo/configcenter/client/ConfigcenterApplication.java
+++ config-center-client/src/main/java/com/demo/configcenter/client/ConfigcenterApplication.java
@@ -1,7 +1,13 @@
-package com.demo.configcenter;
+package com.demo.configcenter.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lingang.zhao
+ * @version 1.0
+ * @date 2019/10/14 9:50 下午
+ */
 
 @SpringBootApplication
 public class ConfigcenterApplication {
---
+++ config-center-common/src/main/java/com/demo/configcenter/common/ConfigCenterApplication.java
@@ -0,0 +1,18 @@
+package com.demo.configcenter.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lingang.zhao
+ * @version 1.0
+ * @date 2019/10/14 9:50 下午
+ */
+@SpringBootApplication
+public class ConfigCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigCenterApplication.class, args);
+	}
+
+}
